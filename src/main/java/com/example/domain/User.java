8bd2a8b80c450a5dec7b4d988c424b0c33178319
Lei.Yang <2031291160@qq.com>
2022-08-14 15:27:09 +0800

    test

---
+++ src/main/java/com/example/domain/User.java
@@ -0,0 +1,21 @@
+package com.example.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author 20312
+ * @author 2022/8/12 19:07
+ */
+@Data
+@Entity//代表实体类
+@Table(name ="user" )//对应user表
+public class User {
+    int id;
+    String userName;
+    String password;
+
+
+}
