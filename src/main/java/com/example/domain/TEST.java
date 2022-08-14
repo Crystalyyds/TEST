dedac6f106fe57a8db08f94c7f72e8cffc016980
Lei.Yang <2031291160@qq.com>
2022-08-14 15:38:35 +0800

    test2

---
+++ src/main/java/com/example/domain/TEST.java
@@ -0,0 +1,11 @@
+package com.example.domain;
+
+/**
+ * @author 20312
+ * @author 2022/8/14 16:22
+ */
+public class TEST {
+    public static void main(String[] args) {
+        System.out.println("hell0 .......");
+    }
+}
--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -22,4 +22,5 @@ public class User {
         this.userName = userName;
         this.password = password;
     }
+
 }
